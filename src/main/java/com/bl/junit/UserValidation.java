8f32317ec86046f1453da1721f90c57753677d1d
sushant2931 <sushantlad8888@gmail.com>
2021-09-16 18:36:52 +0530

    Added:Validation of All Firstname,lastname,Email,Password,Mobile Number Format

---
+++ src/main/java/com/bl/junit/UserValidation.java
@@ -0,0 +1,27 @@
+package com.bl.junit;
+
+public class UserValidation {
+    public boolean validateName(String name) {
+
+        String nameRegex = "^[A-Z]{1}[a-z]{2,}$";
+        return name.matches(nameRegex);
+    }
+    public boolean validateEmail(String email) {
+
+        String emailRegex = "^[a-zA-Z]+[a-zA-Z0-9]*[- . + _]?[a-zA-Z0-9]+[@]{1}[a-z0-9]+[.]{1}[a-z]+[.]?[a-z]+$";
+        return email.matches(emailRegex);
+    }
+
+    public boolean validateMobileNumber(String mobileNumber) {
+
+        String mobileNumberRegex = "^[0-9]{1,2}\\s{1}[0-9]{10}$";
+        return mobileNumber.matches(mobileNumberRegex);
+    }
+
+    public boolean validatePassword(String password) {
+
+        String passwordRegex = "^(?=.*[A-Z])(?=.*[0-9])(?=.{8,}$)[a-zA-Z0-9]*[@#$%^&-+=()][a-zA-Z0-9]*$";
+        return password.matches(passwordRegex);
+    }
+
+}
---
+++ src/test/java/com/bl/junit/UserValidationTest.java
@@ -0,0 +1,142 @@
+package com.bl.junit;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class UserValidationTest {
+    @Test
+    public void givenFirstName_WhenProper_ShouldReturnTrue() {
+
+        UserValidation userValidator = new UserValidation();
+        boolean isValid = userValidator.validateName("Sushant");
+        Assert.assertTrue(isValid);
+
+    }
+
+    @Test
+    public void givenFirstName_WhenFirstLetterIsSmall_ShouldReturnFalse() {
+
+        UserValidation userValidator = new UserValidation();
+        boolean isNotValid = userValidator.validateName("sushant");
+        Assert.assertFalse(isNotValid);
+
+    }
+
+    @Test
+    public void givenFirstName_WhenLesserThanThreeCharacters_ShouldReturnFalse() {
+
+        UserValidation userValidator = new UserValidation();
+        boolean isNotValid = userValidator.validateName("Su");
+        Assert.assertFalse(isNotValid);
+
+    }
+
+    @Test
+    public void givenLastName_WhenProper_ShouldReturnTrue() {
+
+        UserValidation userValidator = new UserValidation();
+        boolean isValid = userValidator.validateName("Lad");
+        Assert.assertTrue(isValid);
+
+    }
+
+    @Test
+    public void givenLastName_WhenFirstLetterIsSmall_ShouldReturnFalse() {
+
+        UserValidation userValidator = new UserValidation();
+        boolean isNotValid = userValidator.validateName("lad");
+        Assert.assertFalse(isNotValid);
+
+    }
+
+    @Test
+    public void givenLastName_WhenLesserThanThreeCharacters_ShouldReturnFalse() {
+
+        UserValidation userValidator = new UserValidation();
+        boolean isNotValid = userValidator.validateName("La");
+        Assert.assertFalse(isNotValid);
+
+    }
+
+    @Test
+    public void givenEmailAddress_WhenProper_ShouldReturnTrue() {
+
+        UserValidation userValidator = new UserValidation();
+        boolean isValid = userValidator.validateEmail("sushantlad8888@gmail.com");
+        Assert.assertTrue(isValid);
+
+    }
+
+    @Test
+    public void givenEmailAddress_WhenNotProper_ShouldReturnFalse() {
+
+        UserValidation userValidator = new UserValidation();
+        boolean isNotValid = userValidator.validateEmail("abc()*@gmail.com");
+        Assert.assertFalse(isNotValid);
+
+    }
+
+    @Test
+    public void givenMobileNumber_WhenProper_ShouldReturnTrue() {
+
+        UserValidation userValidator = new UserValidation();
+        boolean isValid = userValidator.validateMobileNumber("91 8830602356");
+        Assert.assertTrue(isValid);
+    }
+
+    @Test
+    public void givenMobileNumber_WhenNoSpace_ShouldReturnFalse() {
+
+        UserValidation userValidator = new UserValidation();
+        boolean isNotValid = userValidator.validateMobileNumber("918830602356");
+        Assert.assertFalse(isNotValid);
+    }
+
+    @Test
+    public void givenMobileNumber_WhenNumberLessThanTenDigits_ShouldReturnFalse() {
+
+        UserValidation userValidator = new UserValidation();
+        boolean isNotValid = userValidator.validateMobileNumber("91 0602356");
+        Assert.assertFalse(isNotValid);
+    }
+
+    @Test
+    public void givenPassword_WhenProper_ShouldReturnTrue() {
+
+        UserValidation userValidator = new UserValidation();
+        boolean isValid = userValidator.validatePassword("Sushantla@d11");
+        Assert.assertTrue(isValid);
+    }
+
+    @Test
+    public void givenPassword_WhenLessThanEightCharcters_ShouldReturnFalse() {
+
+        UserValidation userValidator = new UserValidation();
+        boolean isNotValid = userValidator.validatePassword("sd@A12");
+        Assert.assertFalse(isNotValid);
+    }
+
+    @Test
+    public void givenPassword_WhenNoUpperCaseCharacter_ShouldReturnFalse() {
+
+        UserValidation userValidator = new UserValidation();
+        boolean isNotValid = userValidator.validatePassword("asjd23$df");
+        Assert.assertFalse(isNotValid);
+    }
+
+    @Test
+    public void givenPassword_WhenNoNumericalNumber_ShouldReturnFalse() {
+
+        UserValidation userValidator = new UserValidation();
+        boolean isNotValid = userValidator.validatePassword("asjdAG$df");
+        Assert.assertFalse(isNotValid);
+    }
+
+    @Test
+    public void givenPassword_WhenMoreThanOneSpecialCharacter_ShouldReturnFalse() {
+
+        UserValidation userValidator = new UserValidation();
+        boolean isNotValid = userValidator.validatePassword("asjd23^$df");
+        Assert.assertFalse(isNotValid);
+    }
+}
